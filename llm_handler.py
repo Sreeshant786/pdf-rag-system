29c528b0fcb1f912dce1ff76a931aa7238272750
Sreeshant Nair <shrishantnair@gmail.com>
2026-05-21 20:16:39 +0530

    fixed deployment

--- llm_handler.py
+++ llm_handler.py
@@ -1,16 +1,16 @@
 from transformers import pipeline
 
 generator = pipeline(
-    task="text2text-generation",
+    "text2text-generation",
     model="google/flan-t5-small"
 )
 
-def generate_answer(question, retrieved_chunks):
+def generate_answer(question, context_chunks):
 
-    context = "\n\n".join(retrieved_chunks)
+    context = " ".join(context_chunks)
 
     prompt = f"""
-    Answer the question briefly and accurately using the context below.
+    Answer the question using the context below.
 
     Context:
     {context}
@@ -21,9 +21,10 @@ def generate_answer(question, retrieved_chunks):
     Answer:
     """
 
-    result = generator(
+    response = generator(
         prompt,
-        max_new_tokens=100
+        max_length=200,
+        do_sample=False
     )
 
-    return result[0]["generated_text"]
+    return response[0]["generated_text"]
