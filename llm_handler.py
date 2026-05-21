35ca61b89d295ea259f980ee8dd868fd19d9d0cf
Sreeshant Nair <shrishantnair@gmail.com>
2026-05-21 20:09:26 +0530

    Fixed transformers pipeline

--- llm_handler.py
+++ llm_handler.py
@@ -1,23 +1,16 @@
 from transformers import pipeline
 
-# Smaller lightweight model
 generator = pipeline(
-    "text2text-generation",
+    task="text2text-generation",
     model="google/flan-t5-small"
 )
-
 
 def generate_answer(question, retrieved_chunks):
 
     context = "\n\n".join(retrieved_chunks)
 
     prompt = f"""
-    You are an AI assistant.
-
-    Answer the question briefly and accurately using only the provided context.
-
-     If the answer is not found, say:
-    "Answer not found in document."
+    Answer the question briefly and accurately using the context below.
 
     Context:
     {context}
@@ -25,12 +18,12 @@ def generate_answer(question, retrieved_chunks):
     Question:
     {question}
 
-     Short Answer:
+    Answer:
     """
 
     result = generator(
         prompt,
-        max_length=100
+        max_new_tokens=100
     )
 
     return result[0]["generated_text"]
